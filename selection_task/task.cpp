b6a2c3af8747d419cadbf32ae6acefdd202a9ea5
theairvideo@gmail.com <theairvideo@gmail.com>
2017-08-02 23:54:45 +0300

    C++ solution implemented

--- selection_task/task.cpp
+++ selection_task/task.cpp
@@ -2,40 +2,40 @@
 
 int count_lines(const std::string& filename)
 {
-	std::ifstream input_file(filename.c_str());
+    std::ifstream input_file(filename.c_str());
 
-	if ( !input_file.good() ) {
-		std::cout << "File doesn't exists: " << filename << std::endl;
-		return -1;
-	}
+    if ( !input_file.good() ) {
+        std::cout << "File doesn't exists: " << filename << std::endl;
+        return -1;
+    }
 
-	int result = std::count(
-					std::istreambuf_iterator<char>(input_file),
-			  		std::istreambuf_iterator<char>(),
-			  		'\n');
+    int result = std::count(
+                           std::istreambuf_iterator<char>(input_file),
+                           std::istreambuf_iterator<char>(),
+                           '\n');
 
-	assert(result >= 0);
+    assert(result >= 0);
 
-	return result;
+    return result;
 }
 
 int main(int argc, char ** argv)
 {
-	if ( argc != 2 ) {
-		std::cout << "Wrong argument format! " << std::endl;
-		std::cout << "./counter <filename>"    << std::endl;
-		return -1;
-	}
+    if ( argc != 2 ) {
+        std::cout << "Wrong argument format! " << std::endl;
+        std::cout << "./counter <filename>"    << std::endl;
+        return -1;
+    }
 
-	std::string file(argv[1]);
-	int lines_amount = count_lines(file);
+    std::string file(argv[1]);
+    int lines_amount = count_lines(file);
 
-	if ( lines_amount == -1 ) {
-		std::cout << "Error occured, try again " << std::endl;
-		return - 1;
-	}
+    if ( lines_amount == -1 ){
+        std::cout << "Error occured, try again " << std::endl;
+        return - 1;
+    }
 
-	std::cout << lines_amount << " " << file << std::endl;
+    std::cout << lines_amount << " " << file << std::endl;
 
-	return 0;
+    return 0;
 }
